5514a26ca5b4c44f8cd82f16d67932ed9d248acd
LemonKing616 <115544224+LemonKing616@users.noreply.github.com>
2023-10-19 11:20:27 +0200

    porfavor

---
+++ src/TablasDeMultiplicar.java
@@ -0,0 +1,11 @@
+public class TablasDeMultiplicar {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Tabla del " + i);
+            for (int j = 0; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
+    }
+}
